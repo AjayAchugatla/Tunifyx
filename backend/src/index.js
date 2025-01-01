0152ea8758d37337589494c855c13a6af121718b
AjayAchugatla <ajayachugatla17@gmail.com>
2025-01-01 07:21:55 +0530

    initialize frontend and backend structure with routing and configuration files

---
+++ backend/src/index.js
@@ -0,0 +1,25 @@
+import express from "express"
+import dotenv from "dotenv"
+import userRoutes from "./routes/userRoutes.js"
+import adminRoutes from "./routes/adminRoutes.js"
+import authRoutes from "./routes/authRoutes.js"
+import songRoutes from "./routes/songRoutes.js"
+import albumRoutes from "./routes/albumRoutes.js"
+import statRoutes from "./routes/statRoutes.js"
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT;
+
+app.use("/api/users", userRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/songs", songRoutes);
+app.use("/api/albums", albumRoutes);
+app.use("/api/stats", statRoutes);
+
+
+app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
+})
