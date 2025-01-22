14c820ff104df0a2d65b740aab785e2e1ed3a862
AjayAchugatla <ajayachugatla17@gmail.com>
2025-01-22 23:34:43 +0530

    add MainLayout and LeftSidebar components, implement resizable panels and scroll area, and create PlaylistSkeleton for loading state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,17 +2,21 @@ import { Route, Routes } from "react-router-dom"
 import Home from "./Pages/Home"
 import AuthCallBack from "./Pages/AuthCallBack"
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react"
+import MainLayout from "./layout/MainLayout"
 
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/sso-callback" element={<AuthenticateWithRedirectCallback
           signUpForceRedirectUrl={"/auth-callback"}
         />} />
         <Route path="/auth-callback" element={<AuthCallBack />} />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+
+        </Route>
       </Routes>
     </>
   )
---
+++ frontend/src/layout/MainLayout.tsx
@@ -0,0 +1,24 @@
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable"
+import { Outlet } from "react-router-dom"
+import LeftSidebar from "./components/LeftSidebar"
+
+const MainLayout = () => {
+    const isMobile = false
+    return (
+        <div className="h-screen bg-black text-white flex flex-col">
+            <ResizablePanelGroup direction="horizontal" className="flex-1 flex h-full overflow-hidden">
+
+                <ResizablePanel defaultSize={20} minSize={isMobile ? 0 : 10} maxSize={20}>
+                    <LeftSidebar />
+                </ResizablePanel>
+                <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
+
+                <ResizablePanel defaultSize={isMobile ? 80 : 60} >
+                    <Outlet />
+                </ResizablePanel>
+            </ResizablePanelGroup>
+        </div>
+    )
+}
+
+export default MainLayout
